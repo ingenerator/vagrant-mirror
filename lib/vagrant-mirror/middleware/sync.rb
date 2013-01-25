19681d65e2ec740535aa820f2a441fb8e7c42640
Andrew Coulton <andrewnfcoulton@gmail.com>
2013-01-25 23:35:26 +0000

    Implement Vagrant::Mirror::Middleware::Sync
    The sync middleware runs a full synchronisation across the mirror,
    transferring new or changed files to the guest or host as required.
    Files that have been deleted from one machine will be treated as
    missing files and reinstated.

--- lib/vagrant-mirror/middleware/sync.rb
+++ lib/vagrant-mirror/middleware/sync.rb
@@ -1,9 +1,58 @@
+# Executes a full sync between the host and guest instance based on the configuration
+# in the vagrantfile, copying new or changed files to the other side of the mirror.
+#
+# @author Andrew Coulton < andrew@ingerator.com >
 module Vagrant
   module Mirror
     module Middleware
       class Sync
 
-        def call
+        # Creates an instance
+        #
+        # @param [Object] The next middleware in the chain
+        # @param [Vagrant::Action::Environment] The environment
+        def initialize(app, env)
+          @app = app
+          @env = env
+        end
+
+        # Executes the middleware and then continues to the next middleware in the
+        # stack
+        #
+        # @param [Vagrant::Action::Environment] The environment
+        def call(env)
+          folders = env[:vm].config.mirror.folders
+          if !folders.empty?
+            synchronize(folders, env)
+          end
+          @app.call(env)
+        end
+
+        protected
+
+        # Synchronizes the folder pairs configured in the vagrantfile
+        #
+        # @param [Array] The folder pairs to synchronise
+        # @param [Vagrant::Action::Environment] The environment
+        def synchronize(folders, env)
+          ui = env[:ui]
+          ui.info("Beginning directory synchronisation")
+
+          connection = Vagrant::Mirror::Connection::SFTP.new(env[:vm], ui)
+
+          folders.each do | folder |
+            if folder[:host_path] == :vagrant_root
+              host_path = env[:root_path]
+            else
+              host_path = folder[:host_path]
+            end
+            sync = Vagrant::Mirror::Sync::All.new(connection, host_path, folder[:guest_path], ui)
+            sync.execute("/")
+          end
+
+          connection.finish_transfers
+          connection.close
+          ui.success("Completed directory synchronisation")
         end
 
       end
--- spec/vagrant-mirror/middleware/sync_spec.rb
+++ spec/vagrant-mirror/middleware/sync_spec.rb
@@ -1,45 +1,170 @@
 describe Vagrant::Mirror::Middleware::Sync do
+  # Shared mocks
+  let (:env)           { Vagrant::Action::Environment.new }
+  let (:vm)            { double("Vagrant::VM").as_null_object }
+  let (:ui)            { double("Vagrant::UI::Interface").as_null_object }
+  let (:app)           { double("Object").as_null_object }
+  let (:config)        { double("Object").as_null_object }
+  let (:configmirror)  { double("Vagrant::Mirror::Config").as_null_object }
+  let (:connection)    { double("Vagrant::Mirror::Connection::SFTP").as_null_object }
+
+  # Set basic stubs for shared mocks
+  before (:each) do
+    env[:vm] = vm
+    env[:ui] = ui
+
+    vm.stub(:config).and_return config
+    config.stub(:mirror).and_return configmirror
+
+    app.stubb(:call)
+
+    Vagrant::Mirror::Connection::SFTP.stub(:new).and_return(connection)
+    Vagrant::Mirror::Sync::All.stub(:new) { double("Vagrant::Mirror::Sync::All") }
+  end
+
+  subject { Vagrant::Mirror::Middleware::Sync.new(app, env) }
+
   describe "#call" do
-    context "with no mirrored folders" do
-      it "calls the next middleware"
+
+    shared_examples "chained middleware" do
+      it "calls the next middleware" do
+        app.should_receive(:call).with(env)
+
+        subject.call(env)
+      end
     end
-    
+
+    context "with no mirrored folders" do
+      before (:each) do
+        configmirror.stub(:folders).and_return([])
+      end
+
+      it_behaves_like "chained middleware"
+    end
+
     context "with mirrored folders" do
-      it "loads ssh params from env"
-      it "loads folders from config"
-      it "creates a connection to the guest"
-      it "logs the start of mirroring"
-      it "logs the end of mirroring"
-      it "waits until all transfers are complete"
-      it "calls the next middleware"
-      
+      let (:syncall1)       { double("Vagrant::Mirror::Sync::All").as_null_object }
+      let (:syncall2)       { double("Vagrant::Mirror::Sync::All").as_null_object }
+
+      shared_examples "beginning transfers" do
+        it "creates a connection to the guest" do
+          Vagrant::Mirror::Connection::SFTP.should_receive(:new)
+            .with(vm, ui)
+            .and_return(connection)
+
+          subject.call(env)
+        end
+
+        it "logs the start of mirroring" do
+          ui.should_receive(:info).with("Beginning directory synchronisation")
+
+          subject.call(env)
+        end
+      end
+
+      shared_examples "folder synchronisation" do | host_path, guest_path, syncseq |
+        it "creates a sync all class" do
+          Vagrant::Mirror::Sync::All.should_receive(:new)
+            .with(connection, host_path, guest_path, ui)
+            .and_return(syncall1)
+
+          subject.call(env)
+        end
+
+        it "runs the sync all action" do
+          if syncseq == 1
+            syncall1.should_receive(:execute).with("/")
+          elsif syncseq == 2
+            syncall2.should_receive(:execute).with("/")
+          else
+            pending
+          end
+
+          subject.call(env)
+        end
+      end
+
+      shared_examples "completing transfers" do
+        it "waits until all transfers are complete" do
+          connection.should_receive(:finish_transfers)
+
+          subject.call(env)
+        end
+
+        it "logs the end of mirroring" do
+          ui.should_receive(:success).with("Completed directory synchronisation")
+
+          subject.call(env)
+        end
+
+        it "closes the connection" do
+          connection.should_receive(:close)
+
+          subject.call(env)
+        end
+
+        it_behaves_like "chained middleware"
+      end
+
+      context "with defined paths" do
+        before (:each) do
+          configmirror.stub(:folders).and_return([
+            {:host_path => 'c:/host', :guest_path => '/var/guest' }
+          ])
+
+          Vagrant::Mirror::Sync::All.stub(:new)
+            .with(connection, 'c:/host', '/var/guest', ui)
+            .and_return(syncall1)
+        end
+
+        it_behaves_like "beginning transfers"
+        it_behaves_like "folder synchronisation", "c:/host", "/var/guest", 1
+        it_behaves_like "completing transfers"
+      end
+
+      context "when mirroring the vagrant root" do
+        before (:each) do
+          configmirror.stub(:folders).and_return([
+            {:host_path => :vagrant_root, :guest_path => '/var/vagrant' }
+          ])
+
+          env[:root_path] = 'c:/vagrant'
+
+          Vagrant::Mirror::Sync::All.stub(:new)
+            .with(connection, 'c:/vagrant', '/var/vagrant', ui)
+            .and_return(syncall1)
+        end
+
+        it_behaves_like "beginning transfers"
+        it_behaves_like "folder synchronisation", "c:/vagrant", "/var/vagrant", 1
+        it_behaves_like "completing transfers"
+      end
+
+      context "with two mirror pairs" do
+        before (:each) do
+          configmirror.stub(:folders).and_return([
+            {:host_path => 'c:/host1', :guest_path => '/var/guest1' },
+            {:host_path => 'c:/host2', :guest_path => '/var/guest2' }
+          ])
+
+          Vagrant::Mirror::Sync::All.stub(:new)
+            .with(connection, 'c:/host1', '/var/guest1', ui)
+            .and_return(syncall1)
+
+          Vagrant::Mirror::Sync::All.stub(:new)
+            .with(connection, 'c:/host2', '/var/guest2', ui)
+            .and_return(syncall2)
+        end
+
+        it_behaves_like "beginning transfers"
+        it_behaves_like "folder synchronisation", "c:/host1", "/var/guest1", 1
+        it_behaves_like "folder synchronisation", "c:/host2", "/var/guest2", 2
+        it_behaves_like "completing transfers"
+      end
+
       context "when the guest connection fails" do
         it "logs the failure"
         it "terminates the vagrant execution with an error"
-      end
-      
-      shared_examples "folder synchronisation" do | host_path, guest_path |
-        it "creates a sync all class"
-        it "passes the connection"
-        it "passes the ui"
-        it "passes the host path"
-        it "passes the guest path"
-        it "runs the sync all action"
-
-      end
-      
-      context "with defined paths" do
-        it_behaves_like "folder synchronisation", "c:/host", "/var/guest"
-      end
-      
-      context "when mirroring the vagrant root" do
-        it "maps the vagrant root path"
-        it_behaves_like "folder synchronisation", "c:/vagrant", "/var/vagrant"
-      end
-      
-      context "with two mirror pairs" do
-        it_behaves_like "folder synchronisation", "c:/host1", "/var/host1"
-        it_behaves_like "folder synchronisation", "c:/host2", "/var/host2"
       end
     end
   end
