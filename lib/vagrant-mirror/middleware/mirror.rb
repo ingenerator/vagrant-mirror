a73ecf9fb5c73dc5b8f16b14cef3dd6f88680b0a
Andrew Coulton <andrewnfcoulton@gmail.com>
2013-01-26 18:17:09 +0000

    Initial implementation of Vagrant::Mirror::Monitor
    Creates a thread for each pair of folders and starts listening for
    changes, transferring changed files between the source and destination
    as required.

--- lib/vagrant-mirror/middleware/mirror.rb
+++ lib/vagrant-mirror/middleware/mirror.rb
@@ -1,9 +1,83 @@
+# Monitors changes on the host and guest instance, and propogates any new, changed
+# or deleted files between machines. Note that this will block the vagrant
+# execution on the host.
+#
+# @author Andrew Coulton < andrew@ingerator.com >
 module Vagrant
   module Mirror
     module Middleware
       class Mirror
 
-        def call
+        # Creates an instance
+        #
+        # @param [Object] The next middleware in the chain
+        # @param [Vagrant::Action::Environment] The environment
+        def initialize(app, env)
+          @app = app
+          @env = env
+        end
+
+        # Executes the middleware and then continues to the next middleware in the
+        # stack
+        #
+        # @param [Vagrant::Action::Environment] The environment
+        def call(env)
+          folders = env[:vm].config.mirror.folders
+          if !folders.empty?
+            mirror(folders, env)
+          else
+            env[:ui].info("No vagrant-mirror mirrored folders configured for this box")
+          end
+          @app.call(env)
+        end
+
+        protected
+
+        # Mirrors the folder pairs configured in the vagrantfile
+        #
+        # @param [Array] The folder pairs to synchronise
+        # @param [Vagrant::Action::Environment] The environment
+        def mirror(folders, env)
+          ui = env[:ui]
+          ui.info("Beginning directory mirroring")
+
+          connection = Vagrant::Mirror::Connection::SFTP.new(env[:vm], ui)
+          threads = []
+
+          folders.each do | folder |
+            # Create a new main thread to poll for changes on each folder pairing
+            threads << Thread.new do
+              if folder[:host_path] == :vagrant_root
+                host_path = env[:root_path]
+              else
+                host_path = folder[:host_path]
+              end
+
+              sync = Vagrant::Mirror::Sync::Changes.new(connection, host_path, folder[:guest_path], ui)
+              Thread.current["queue"] = Queue.new
+              host_listener = Vagrant::Mirror::Listen::Host.new(host_path, Thread.current["queue"])
+
+              Thread.current["host_listener_thread"] = host_listener.listen
+
+              # Just poll indefinitely waiting for changes or to be told to quit
+              quit = false
+              while !quit
+                change = Thread.current["queue"].pop
+                if (change[:quit])
+                  quit = true
+                else
+                  sync.execute(change[:source], change[:added], change[:modified], change[:removed])
+                end
+              end
+            end
+          end
+
+          # Wait for the threads to exit
+          threads.each do | thread |
+            thread.join
+          end
+
+          ui.success("Completed directory synchronisation")
         end
 
       end
--- spec/vagrant-mirror/middleware/mirror_spec.rb
+++ spec/vagrant-mirror/middleware/mirror_spec.rb
@@ -1,80 +1,184 @@
 describe Vagrant::Mirror::Middleware::Mirror do
+  # Shared mocks
+  let (:env)           { Vagrant::Action::Environment.new }
+  let (:vm)            { double("Vagrant::VM").as_null_object }
+  let (:ui)            { double("Vagrant::UI::Interface").as_null_object }
+  let (:app)           { double("Object").as_null_object }
+  let (:config)        { double("Object").as_null_object }
+  let (:configmirror)  { double("Vagrant::Mirror::Config").as_null_object }
+  let (:connection)    { double("Vagrant::Mirror::Connection::SFTP").as_null_object }
+  let (:syncchanges)   { double("Vagrant::Mirror::Sync::Changes").as_null_object }
+  let (:queue)         { double("Queue").as_null_object }
+  let (:guard)         { double("Vagrant::Mirror::Listen::Host").as_null_object }
+
+  # Set basic stubs for shared mocks
+  before (:each) do
+    env[:vm] = vm
+    env[:ui] = ui
+
+    vm.stub(:config).and_return config
+    config.stub(:mirror).and_return configmirror
+
+    app.stub(:call)
+
+    Vagrant::Mirror::Connection::SFTP.stub(:new).and_return(connection)
+    Vagrant::Mirror::Sync::Changes.stub(:new).and_return(syncchanges)
+    Vagrant::Mirror::Listen::Host.stub(:new).and_return(guard)
+    Queue.stub(:new).and_return(queue)
+
+  end
+
+  subject { Vagrant::Mirror::Middleware::Mirror.new(app, env) }
+
   describe "#call" do
+    shared_examples "chained mirror middleware" do
+      it "calls the next middleware" do
+        app.should_receive(:call).with(env)
+
+        subject.call(env)
+      end
+    end
+
     context "with no mirrored folders" do
-      it "calls the next middleware"
+      before (:each) do
+        configmirror.stub(:folders).and_return([])
+      end
+
+      it_behaves_like "chained mirror middleware"
     end
 
     context "with mirrored folders" do
-      it "loads ssh params from env"
-      it "loads folders from config"
-      it "creates a connection to the guest"
-      it "opens a new TCP notification listener"
 
-      context "when the guest connection fails" do
-        it "logs the failure"
-        it "terminates the vagrant execution with an error"
+      shared_examples "beginning mirroring" do
+        it "creates a connection to the guest" do
+          Vagrant::Mirror::Connection::SFTP.should_receive(:new)
+            .with(vm, ui)
+            .and_return(connection)
+
+          subject.call(env)
+        end
+
+        it "logs the start of mirroring" do
+          ui.should_receive(:info).with("Beginning directory mirroring")
+
+          subject.call(env)
+        end
+
+        it "opens a new TCP notification listener"
+
+        context "when the guest connection fails" do
+          it "logs the failure"
+          it "terminates the vagrant execution with an error"
+        end
+
+        context "when the TCP listener cannot start" do
+          it "logs the failure"
+          it "terminates the vagrant execution with an error"
+        end
+
       end
 
-      context "when the TCP listener cannot start" do
-        it "logs the failure"
-        it "terminates the vagrant execution with an error"
-      end
+      shared_examples "processing changes" do | host_path, guest_path |
+        it "creates a sync changes class" do
+          Vagrant::Mirror::Sync::Changes.should_receive(:new)
+            .with(connection, host_path, guest_path, ui)
+            .and_return(syncchanges)
 
-      shared_examples "creating a mirror class" do | host_path, guest_path |
-        it "creates a sync::mirror"
-        it "passes the connection"
-        it "passes the ui"
-        it "passes the host path"
-        it "passes the guest path"
-      end
+          subject.call(env)
+        end
 
-      shared_examples "creating a guard listener" do | host_path, guest_path |
-        it "listens for filesystem changes in the host path"
+        it "creates a Guard listener" do
+          Vagrant::Mirror::Listen::Host.should_receive(:new)
+            .with(host_path, queue)
+
+          subject.call(env)
+        end
+
+        it "listens for changes in the host path" do
+          guard.should_receive(:listen)
+
+          subject.call(env)
+        end
+
+        context "with notifications in the queue" do
+          let (:added)    { ['added'] }
+          let (:modified) { ['modified','modified2'] }
+          let (:removed)  { ['removed'] }
+
+          before (:each) do
+            queue.stub(:pop).and_return(
+              { :source => :host, :added => added, :modified => modified, :removed => removed },
+              { :source => :guest, :added => added, :modified => modified, :removed => removed },
+              { :quit => true })
+
+          end
+
+          it "synchronises the first changes" do
+            syncchanges.should_receive(:execute)
+              .with(:host, added, modified, removed)
+
+            subject.call(env)
+            sleep 0.2
+          end
+
+          it "synchronises the second changes" do
+            syncchanges.should_receive(:execute)
+              .with(:guest, added, modified, removed)
+
+            subject.call(env)
+            sleep 0.2
+          end
+
+        end
+
+        shared_examples "finishing mirroring" do
+          it "signals the TCP listener to quit"
+          it "signals the Guard listener to quit"
+          it "waits for the TCP listener to quit"
+          it "waits for the Guard listener to quit"
+          it "processes remaining jobs in the queue"
+          it "calls the next middleware"
+        end
+
+        context "when signaled to quit" do
+          it_behaves_like "finishing mirroring"
+        end
+
+        context "when user presses q on the console" do
+          it_behaves_like "finishing mirroring"
+        end
       end
 
       context "with defined paths" do
-        it_behaves_like "creating a mirror class", 'C:/host','/var/guest'
-        it_behaves_like "creating a guard listener", 'C:/host','/var/guest'
+        before(:each) do
+          configmirror.stub(:folders).and_return([
+            {:host_path => 'c:/host', :guest_path => '/var/guest' }
+          ])
+        end
+
+        it_behaves_like "beginning mirroring"
+        it_behaves_like "processing changes", 'c:/host', '/var/guest'
+        it_behaves_like "chained mirror middleware"
       end
 
       context "when mirroring the vagrant root" do
-        it_behaves_like "creating a mirror class", 'C:/host','/var/guest'
-        it_behaves_like "creating a guard listener", 'C:/host','/var/guest'
+        before (:each) do
+          configmirror.stub(:folders).and_return([
+            {:host_path => :vagrant_root, :guest_path => '/var/vagrant' }
+          ])
+
+          env[:root_path] = 'c:/vagrant'
+        end
+
+        it_behaves_like "beginning mirroring"
+        it_behaves_like "processing changes", 'c:/vagrant', '/var/vagrant'
+        it_behaves_like "chained mirror middleware"
       end
 
-      context "with two mirror pairs" do
-        it_behaves_like "creating a mirror class", 'C:/host1','/var/guest1'
-        it_behaves_like "creating a guard listener", 'C:/host1','/var/guest1'
-        it_behaves_like "creating a mirror class", 'C:/host2','/var/guest2'
-        it_behaves_like "creating a guard listener", 'C:/host2','/var/guest2'
+      context "when mirroring multiple folders" do
+        pending
       end
 
-      context "with notifications in the queue" do
-        it "synchronises the changes"
-        it "waits for more notifications"
-      end
-
-      context "with no changes in the queue" do
-        it "waits for new notifications"
-        it "runs until signaled"
-      end
-
-      shared_examples "an orderly shutdown" do
-        it "signals the TCP listener to quit"
-        it "signals the Guard listener to quit"
-        it "waits for the TCP listener to quit"
-        it "waits for the Guard listener to quit"
-        it "processes remaining jobs in the queue"
-        it "calls the next middleware"
-      end
-
-      context "when signaled to quit" do
-        it_behaves_like "an orderly shutdown"
-      end
-
-      context "when user presses q on the console" do
-        it_behaves_like "an orderly shutdown"
-      end
     end
   end
 end
