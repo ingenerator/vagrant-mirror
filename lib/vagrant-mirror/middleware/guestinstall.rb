d71fe14a826ddcf3af107d2d13f76ea2b864bfff
Andrew Coulton <andrewnfcoulton@gmail.com>
2013-01-27 15:34:06 +0000

    Missing base method in GuestInstall

--- lib/vagrant-mirror/middleware/guestinstall.rb
+++ lib/vagrant-mirror/middleware/guestinstall.rb
@@ -3,6 +3,14 @@ module Vagrant
     module Middleware
       class GuestInstall < Base
 
+      protected
+
+        # Synchronizes the folder pairs configured in the vagrantfile
+        #
+        # @param [Array] The folder pairs to synchronise
+        # @param [Vagrant::Action::Environment] The environment
+        def execute(mirrors, env)
+        end
       end
     end
   end
