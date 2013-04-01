83579292ffe70f601662ce75639e55e6c987413b
Andrew Coulton <andrewnfcoulton@gmail.com>
2013-04-01 18:20:51 +0100

    Implement the Vagrant::Mirror::Rsync class
    Manages the transfer of files on the guest between the virtualbox
    shared folder and the destination path. Currently runs in verbose
    but likely to be scaled back once we are confident it is working.

--- lib/vagrant-mirror/rsync.rb
+++ lib/vagrant-mirror/rsync.rb
@@ -5,25 +5,85 @@
 module Vagrant
   module Mirror
     class Rsync
+      # @return [Vagrant::VM] The VM to mirror on
       attr_reader :vm
+
+      # @return [String] The path to the virtualbox shared folder on the guest
       attr_reader :guest_sf_path
+
+      # @return [String] The path to the virtualbox shared folder on the host
       attr_reader :host_path
+
+      # @return [Bool] Whether Rsync should delete unexpected files
       attr_reader :delete
+
+      # @return [Array] The array of paths to exclude
       attr_reader :excludes
+
+      # @return [String] The exclude paths formatted as an array of rsync exclude arguments
+      attr_reader :exclude_args
+
+      # @return [String] The path to the mirror folder on the guest
       attr_reader :guest_path
 
+      # Creates an instance
+      #
+      # @param [Vagrant::VM] The VM to mirror on
+      # @param [String]      The path of the shared folder on the guest to use as the rsync source
+      # @param [String]      The path of the shared folder on the host - used to check whether a path is a directory
+      # @param [Hash]        The config.mirror options hash with the guest mirror destination, delete, etc
       def initialize(vm, guest_sf_path, host_path, mirror_config)
         @vm = vm
         @guest_sf_path = guest_sf_path
         @host_path = host_path
         @delete = mirror_config[:delete]
-        @excludes = mirror_config[:excludes]
+        @excludes = mirror_config[:exclude]
         @guest_path = mirror_config[:guest_path]
+
+        # Build the exclude argument array
+        @exclude_args = []
+        if (@excludes.count > 0)
+          @excludes.each do | exclude |
+            exclude_args << "--exclude '#{exclude}'"
+          end
+        end
       end
 
+      # Run rsync on the guest to update a path - either the whole mirror directory or individual
+      # files and folders within it.
+      #
+      # @param [String] The path to run in
       def run(path)
-        # Build the source and destination
+        # Strip a leading / off the path to avoid any problems
+        path.sub!(/^\//, '')
 
+        # Build the source and destination paths
+        source = "#{guest_sf_path}/#{path}"
+        dest = "#{guest_path}/#{path}"
+
+        # Check if the source is a directory on the host - if so, add a / for rsync
+        if ((path != '') && (File.directory?(File.join(host_path, path))))
+          source << '/'
+          dest << '/'
+        end
+
+
+        # Build the rsync command
+        args = ['rsync -av']
+
+        if (delete)
+          args << '--del'
+        end
+
+        args = args + exclude_args
+
+        args << source
+        args << dest
+
+        cmd = args.join(' ')
+
+        # Run rsync
+        vm.channel.sudo(cmd)
       end
 
     end
--- spec/vagrant-mirror/rsync_spec.rb
+++ spec/vagrant-mirror/rsync_spec.rb
@@ -1,15 +1,75 @@
 describe Vagrant::Mirror::Rsync do
 
+  # Shared mocks
+  let (:vm)            { double("Vagrant::VM").as_null_object }
+  let (:channel)       { double("Vagrant::Communication::SSH").as_null_object }
+  let (:guest_sf_path) { '/vagrant' }
+  let (:host_path)     { 'c:/vagrant' }
+  let (:cfg_delete)    { false }
+  let (:cfg_exclude)   { [] }
+  let (:cfg_guest)     { '/var/vagrant' }
+  let (:mirror_config) { { :name => 'v-root', :guest_path => '/var/vagrant', :delete => false,
+                           :exclude => [], :beep => false, :symlinks => [] } }
+
+  # Set basic stubs for shared mocks
+  before (:each) do
+    vm.stub(:channel).and_return channel
+
+    File.stub(:directory?).and_return(false)
+    File.stub(:directory?).with('c:/vagrant/dir').and_return(true)
+  end
+
+  subject { Vagrant::Mirror::Rsync.new(vm, guest_sf_path, host_path, mirror_config) }
+
   describe "#run" do
+
+    shared_examples "running rsync to update the path" do | path, expect_source, expect_dest |
+      context "with no exclude paths" do
+        it "runs the expected rsync command" do
+          channel.should_receive(:sudo).with("rsync -av #{expect_source} #{expect_dest}")
+
+          subject.run(path)
+        end
+      end
+
+      context "with exclude paths" do
+        let (:mirror_config) { { :name => 'v-root', :guest_path => '/var/vagrant', :delete => false,
+                           :exclude => ['.git', 'cache'], :beep => false, :symlinks => [] } }
+
+        it "runs the expected rsync command"  do
+          channel.should_receive(:sudo).with("rsync -av --exclude '.git' --exclude 'cache' #{expect_source} #{expect_dest}")
+
+          subject.run(path)
+        end
+      end
+
+      context "with delete enabled" do
+        let (:mirror_config) { { :name => 'v-root', :guest_path => '/var/vagrant', :delete => true,
+                           :exclude => [], :beep => false, :symlinks => [] } }
+
+        it "runs the expected rsync command"  do
+          channel.should_receive(:sudo).with("rsync -av --del #{expect_source} #{expect_dest}")
+
+          subject.run(path)
+        end
+      end
+    end
+
+    context "when passed an empty path" do
+      it_behaves_like "running rsync to update the path", '', '/vagrant/', '/var/vagrant/'
+    end
+
     context "when passed the root path" do
-      it "rsyncs from the top level mirrored folders on the guest"
-      it "excludes any configured exclude paths"
-
+      it_behaves_like "running rsync to update the path", '/', '/vagrant/', '/var/vagrant/'
     end
 
-    context "when passed a child path" do
-      it "rsyncs from the child level mirrored path on the guest"
-      it "excludes any configured exclude paths"
+    context "when passed a file path" do
+      it_behaves_like "running rsync to update the path", 'file', '/vagrant/file', '/var/vagrant/file'
     end
+
+    context "when passed a directory path" do
+      it_behaves_like "running rsync to update the path", 'dir', '/vagrant/dir/', '/var/vagrant/dir/'
+    end
+
   end
 end
